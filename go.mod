c339b215aa346c561ef1c20be5998417c97e8610
MandarM <mandhar30@gmail.com>
2021-12-22 18:30:17 +0100

    Added confluent lib and producer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Mandar07/confl_kafkaGo_Producer
+
+go 1.16
+
+require github.com/confluentinc/confluent-kafka-go v1.7.0
---
+++ producer.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+const (
+	topic  = "selftuts"
+	broker = "localhost:9092"
+)
+
+func main() {
+	ctx := context.Background()
+	produce(ctx)
+}
+
+func produce(ctx context.Context) {
+
+	configMap := make(map[string]kafka.ConfigValue)
+	configMap["bootstrap.servers"] = "localhost:9092" // on host, outside docker container
+	var kConfigMap kafka.ConfigMap = kafka.ConfigMap(configMap)
+
+	p, err := kafka.NewProducer(&kConfigMap)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() { // async ack update
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Message failed to deliver %v \n", ev.TopicPartition.Error.Error())
+				} else {
+					fmt.Printf("Message delivered sucess %v \n", ev.TopicPartition.Offset.String())
+				}
+			}
+		}
+	}()
+
+	topic := "selftuts" // topic must exit or use adminClient to create one
+	topicPartition := kafka.TopicPartition{
+		Topic:     &topic,
+		Partition: 0,
+	}
+
+	keys := []string{"key1", "key2", "key3", "key4", "key5"}
+	values := []string{"value 1", "value 2", "value 3", "value 4", "value 5"}
+
+	for i := range keys {
+
+		kafkaMsg := kafka.Message{TopicPartition: topicPartition, Value: []byte(values[i]), Key: []byte(keys[i])}
+		p.Produce(&kafkaMsg, nil)
+	}
+
+	p.Flush(25000) // wait
+}
